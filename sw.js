8fc05f606902b6c863fe47442a4f25dfbca2d367
zcchen496-code <zcchen496@gmail.com>
2026-08-27 20:13:55 +0800

    Refresh cache for V4 export fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='plant-quadrat-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='plant-quadrat-v4-2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./app-v4.js','./export-v4.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
