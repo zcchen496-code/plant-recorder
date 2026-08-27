749bfaaf774a738de829579c38e2983eca6ee160
zcchen496-code <zcchen496@gmail.com>
2026-08-27 18:04:40 +0800

    Bump cache for continuous voice update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='plant-quadrat-v2';
+const CACHE='plant-quadrat-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
